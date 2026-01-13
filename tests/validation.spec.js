9a40099d191c970a9c24aac2e171fcd3baf41349
KhitishJena <satishkumarjena307@gmail.com>
2026-01-13 20:15:06 +0530

    more validations

---
+++ tests/validation.spec.js
@@ -0,0 +1,19 @@
+import {test, expect} from '@playwright/test';
+
+test("More validations", async({page})=>{
+
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+   // await page.goto("https://google.com");
+   // await page.goBack();            //Navigations
+    //await page.goForward();         //in the browser 
+    //await page.goBack();            //like forward and backward
+
+    await expect(page.locator("#displayed-text")).toBeVisible();
+    await page.locator("#hide-textbox").click();
+    await expect(page.locator("#displayed-text")).toBeHidden();
+
+    await page.locator("#confirmbtn").click();
+    page.on("dialog",dialog => dialog.accept());
+    //page.on("dialog",dialog => dialog.dismiss());
+
+});
